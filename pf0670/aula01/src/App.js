c06fde968e5cd3f21ec650d7a11c896c39654d6c
alecarlosjesus <alexgit@hotmail.com>
2021-08-05 22:29:55 -0300

    projeto exemplo React - Aula01

---
+++ pf0670/aula01/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Cabececalho from './Cabecalho';
+
+function App() {
+
+  const nomeCurso = '1TDS'
+
+  return (
+
+    <div className="App">
+      <Cabececalho/>
+    </div>
+  );
+
+}
+
+export default App;
