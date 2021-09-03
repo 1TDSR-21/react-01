e41dcb0d0a3a87fe55087f1cf65d5ec55d701242
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2021-09-02 21:36:49 -0300

    Renderização condicional e Props.Children.

---
+++ pf0670/react-aula3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Master from './components/master/Master'
+
+export default function App() {
+    return (
+        <>
+            <Master/>
+        </>
+    )
+}
+
+
