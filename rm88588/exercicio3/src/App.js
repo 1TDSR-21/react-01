25974f81f14bdb6eb784d9b0ad54f4fe67cb2870
Andre Hugo Bastos da Silva <andre.hugo.b.silva@accenture.com>
2021-09-09 23:22:51 -0300

    semaforo

---
+++ rm88588/exercicio3/src/App.js
@@ -0,0 +1,10 @@
+import Semaforo from "./components/semaforo/Semaforo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>André Hugo Bastos da Silva - RM 88588 - 1TDSR</h1>
+      <Semaforo />
+    </div>
+  );
+}
---
+++ rm88588/exercicio3/src/components/semaforo/Semaforo.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Semaforo.css";
+
+export default function Semaforo() {
+
+  return(
+    <div>
+      <h2 id="siga">Siga</h2>
+      <h2 id="atencao">Atenção</h2>
+      <h2 id="pare">Pare</h2>
+      <button onClick={() => {
+        if(document.getElementById("siga").style.visibility !== 'hidden') {
+          console.log('testando')
+          document.getElementById("atencao").style.visibility = 'visible';
+          document.getElementById("siga").style.visibility = 'hidden';
+          document.getElementById("pare").style.visibility = 'hidden';
+        } else if (document.getElementById("atencao").style.visibility !== 'hidden') {
+          document.getElementById("atencao").style.visibility = 'hidden';
+          document.getElementById("siga").style.visibility = 'hidden';
+          document.getElementById("pare").style.visibility = 'visible';
+        } else {
+          document.getElementById("atencao").style.visibility = 'hidden';
+          document.getElementById("siga").style.visibility = 'visible';
+          document.getElementById("pare").style.visibility = 'hidden';
+        }
+      }}>Trocar</button>
+    </div>
+  )
+}
