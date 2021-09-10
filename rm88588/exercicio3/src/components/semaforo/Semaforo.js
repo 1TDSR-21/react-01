8282f59d53ef6fe2d2e0ff72393f65115121e25c
Andre Hugo Bastos da Silva <andre.hugo.b.silva@accenture.com>
2021-09-09 23:40:16 -0300

    using hooks

--- rm88588/exercicio3/src/components/semaforo/Semaforo.js
+++ rm88588/exercicio3/src/components/semaforo/Semaforo.js
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Semaforo.css";
 
 export default function Semaforo() {
+
+  const [sinal, setSinal] = useState(0);
+
+  const alternarSinal = () => {
+    if(sinal === 0) {
+      document.getElementById("atencao").style.visibility = 'visible';
+      document.getElementById("siga").style.visibility = 'hidden';
+      document.getElementById("pare").style.visibility = 'hidden';
+      setSinal(1);
+    } else if(sinal === 1) {
+      document.getElementById("atencao").style.visibility = 'hidden';
+      document.getElementById("siga").style.visibility = 'hidden';
+      document.getElementById("pare").style.visibility = 'visible';
+      setSinal(2)
+    } else {
+      document.getElementById("atencao").style.visibility = 'hidden';
+      document.getElementById("siga").style.visibility = 'visible';
+      document.getElementById("pare").style.visibility = 'hidden';
+      setSinal(0);
+    }
+  }
 
   return(
     <div>
       <h2 id="siga">Siga</h2>
       <h2 id="atencao">Atenção</h2>
       <h2 id="pare">Pare</h2>
-      <button onClick={() => {
-        if(document.getElementById("siga").style.visibility !== 'hidden') {
-          console.log('testando')
-          document.getElementById("atencao").style.visibility = 'visible';
-          document.getElementById("siga").style.visibility = 'hidden';
-          document.getElementById("pare").style.visibility = 'hidden';
-        } else if (document.getElementById("atencao").style.visibility !== 'hidden') {
-          document.getElementById("atencao").style.visibility = 'hidden';
-          document.getElementById("siga").style.visibility = 'hidden';
-          document.getElementById("pare").style.visibility = 'visible';
-        } else {
-          document.getElementById("atencao").style.visibility = 'hidden';
-          document.getElementById("siga").style.visibility = 'visible';
-          document.getElementById("pare").style.visibility = 'hidden';
-        }
-      }}>Trocar</button>
+      <button onClick={() => alternarSinal()}>Trocar</button>
     </div>
   )
 }
