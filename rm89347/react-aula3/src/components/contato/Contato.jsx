f8a76fe2d2e4b65f230684d9f5e954e4037e9f85
codeIsrael <israel.vire@gmail.com>
2021-09-09 22:30:32 -0300

    Linpando pasta da aula

--- rm89347/react-aula3/src/components/contato/Contato.jsx
+++ rm89347/react-aula3/src/components/contato/Contato.jsx
@@ -1,19 +1,48 @@
 import iconTel from '../img/tel.png'
 import iconEmail from '../img/mail.png'
+import { useState } from 'react'
 
 export function Contato() {
+    const [dados, setDados] = useState({ 'telefone': '', 'email': '' })
+
+    const alteraDados = (e) => {
+        //Executando uma desestruturação
+        const { name, value } = e.target
+
+        if (name === 'telefone') {
+            setDados({ 'telefone': value, 'email': dados.email })
+        } else if (name === 'email') {
+            setDados({ 'telefone': value, 'email': dados.email })
+        }
+    }
+
     return (
-        <section id="contatos">
+        <section id="contato">
             <h2>Contatos</h2>
             <p>Fale Conosco</p>
             <div>
                 <img src={iconTel} alt="Telefone" />
-                <p><a href="tel:11987654321">(11) 97856-4567</a></p>
+                <p><a href="tel:11978564567">(11) 97856-4567</a></p>
             </div>
             <div>
                 <img src={iconEmail} alt="Email" />
-                <p><a href="mailto:rm89347@fiap.com.br">rm89347@fiap.com.br</a></p>
+                <p><a href="mailto:profalexandre.jesus@fiap.com.br">profalexandre.jesus@fiap.com.br</a></p>
             </div>
-        </section>
+
+            <div>
+                <h2>Preencha seus dados:</h2>
+                Tel  :<input type="text" name="telefone" /> <br />
+                Email:<input type="text" name="email" /> <br />
+            </div>
+            <div>
+                <img src={iconTel} alt="Telefone" />
+                <p><a href="tel:"{...dados.telefone}>{dados.telefone}</a></p>
+            </div>
+            <div>
+                <img src={iconEmail} alt="Email" />
+                <p><a href="mailto:"{...dados.email}>{dados.email}</a></p>
+            </div>
+
+        </section >
     )
 }
